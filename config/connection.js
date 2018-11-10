1e5aed624e3614a0666945cb7c5a638cbf2895ce
Kruges <wesleykruger@gmail.com>
2018-11-09 19:05:15 -0600

    security

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ var mysql = require("mysql");
 require("dotenv").config();
 
 var connection = mysql.createConnection(process.env.JAWSDB_URL, {
-  host: "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: process.env.DB_HOST,
   port: process.env.PORT || 3306,
-  user: "mnunbmmeqjus4o0k",
-  password: "euzxyx2m43rxlmdg",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: "ei74wycbme4eiz8c"
 });
 
